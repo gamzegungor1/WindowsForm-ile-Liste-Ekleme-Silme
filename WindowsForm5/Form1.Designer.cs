6b9f407bae990e905c2a169b3dca5494d5de7423
gamzegungor1 <102431618+gamzegungor1@users.noreply.github.com>
2022-03-31 16:56:08 +0300

    Add files via upload

---
+++ WindowsForm5/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace WindowsForm5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ekle = new System.Windows.Forms.TextBox();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_countBul = new System.Windows.Forms.Button();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eklemek İstediğiniz Değeri Giriniz : ";
+            // 
+            // txt_ekle
+            // 
+            this.txt_ekle.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_ekle.Location = new System.Drawing.Point(348, 32);
+            this.txt_ekle.Name = "txt_ekle";
+            this.txt_ekle.Size = new System.Drawing.Size(167, 22);
+            this.txt_ekle.TabIndex = 1;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_ekle.Location = new System.Drawing.Point(357, 84);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(158, 62);
+            this.btn_ekle.TabIndex = 2;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = false;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_sil.Location = new System.Drawing.Point(357, 152);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(158, 49);
+            this.btn_sil.TabIndex = 3;
+            this.btn_sil.Text = "SİL";
+            this.btn_sil.UseVisualStyleBackColor = false;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // btn_countBul
+            // 
+            this.btn_countBul.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_countBul.Location = new System.Drawing.Point(357, 207);
+            this.btn_countBul.Name = "btn_countBul";
+            this.btn_countBul.Size = new System.Drawing.Size(158, 49);
+            this.btn_countBul.TabIndex = 4;
+            this.btn_countBul.Text = "COUNT BUL";
+            this.btn_countBul.UseVisualStyleBackColor = false;
+            this.btn_countBul.Click += new System.EventHandler(this.btn_countBul_Click);
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_temizle.Location = new System.Drawing.Point(357, 262);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(158, 50);
+            this.btn_temizle.TabIndex = 5;
+            this.btn_temizle.Text = "TEMİZLE";
+            this.btn_temizle.UseVisualStyleBackColor = false;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "OCAK",
+            "ŞUBAT",
+            "MART",
+            "NİSAN",
+            "MAYIS",
+            "HAZİRAN",
+            "TEMMUZ",
+            "AĞUSTOS",
+            "EYLÜL",
+            "EKİM"});
+            this.listBox1.Location = new System.Drawing.Point(17, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(310, 228);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.UseWaitCursor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 423);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_temizle);
+            this.Controls.Add(this.btn_countBul);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.txt_ekle);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ekle;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_countBul;
+        private System.Windows.Forms.Button btn_temizle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ WindowsForm5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            string gun = txt_ekle.Text;
+            listBox1.Items.Add(gun);
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            string silinecekGun = listBox1.SelectedItem.ToString();
+
+            listBox1.Items.Remove(silinecekGun);
+        }
+
+        private void btn_countBul_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Listbox count değeri : " + listBox1.Items.Count.ToString());
+;       }
+
+        private void btn_temizle_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            MessageBox.Show("Listbox temizlendi");
+        }
+    }
+}
